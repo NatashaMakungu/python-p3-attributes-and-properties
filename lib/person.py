e620ebe1e4696c6800231a392dc0418496688d56
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-05-24 18:19:37 +0300

    complete lib

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,55 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    APPROVED_JOBS = [
+        "Admin",
+        "Customer Service",
+        "Human Resources",
+        "ITC",
+        "Production",
+        "Legal",
+        "Finance",
+        "Sales",
+        "General Management",
+        "Research & Development",
+        "Marketing",
+        "Purchasing"
+    ]
+
+    def __init__(self, name='', job=''):
+        self._name = None
+        self._job = None
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in Person.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+person = Person(name="Natasha", job="ITC")
+print(person.name)  # => Natasha
+print(person.job)   # => ITC
+
+person.name = "a very long name that exceeds twenty-five characters"
+# => Name must be string between 1 and 25 characters.
+
+person.job = "Unknown Job"
+# => Job must be in list of approved jobs.
